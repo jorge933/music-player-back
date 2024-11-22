fa3e13d187e3d0857de61c48e7e5de1667fab1b7
Jorge <jorgevitorp9@gmail.com>
2024-11-22 10:32:29 -0300

    📦 refactor(error-handler.middleware): removed unused parameters

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,15 +1,14 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { ApiError } from "../classes/api-error";
+import { HttpStatusCode } from "axios";
 
-export function errorHandler(
-  error: ApiError,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function errorHandler(error: ApiError, req: Request, res: Response) {
   const { statusCode, message, type } = error;
   const responseObj: { [key: string]: string } = { message, type };
 
   console.log(error);
-  res.status(statusCode || 500).json(responseObj);
+
+  res
+    .status(statusCode || HttpStatusCode.InternalServerError)
+    .json(responseObj);
 }
