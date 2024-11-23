eae2c6d8480c0599c2f9baf60e80a0905ccaa6fe
Jorge <jorgevitorp9@gmail.com>
2024-11-23 08:23:37 -0300

    📦 refactor(index): getting controller methods that use routes decorators and setting as app route

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,64 @@
-import express from "express";
 import bodyParser from "body-parser";
+import express from "express";
 
-import "express-async-errors";
 import "dotenv/config";
-
-import { Controller } from "./interfaces/controller";
+import "express-async-errors";
+import "reflect-metadata";
 
 import { errorHandler } from "./middlewares/error-handler.middleware";
 
 import { DownloadSongController } from "./controllers/downloadSong.controller";
 import { SearchSongsController } from "./controllers/searchSongs.controller";
+import {
+  Controller,
+  ControllerRouteMetadata,
+  Metadata,
+} from "./interfaces/route-metadata";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const controllers: Controller[] = [
-  new DownloadSongController(),
-  new SearchSongsController(),
-];
+const controllers = [DownloadSongController, SearchSongsController];
 
-controllers.forEach(({ prefix, router }) => {
-  app.use(prefix, router);
+function getMethodsWithMetadata({ prototype }: Controller) {
+  const properties = Object.getOwnPropertyNames(prototype);
+
+  const routesMetadata = properties.reduce(
+    (previousValue: ControllerRouteMetadata[], property) => {
+      const metadata: Metadata = Reflect.getMetadata(
+        "route",
+        prototype,
+        property
+      );
+
+      if (!metadata) return previousValue;
+
+      return [
+        ...previousValue,
+        { callbackPropertyName: property, ...metadata },
+      ];
+    },
+    []
+  );
+
+  return routesMetadata;
+}
+
+controllers.forEach((controllerClass) => {
+  const router = express.Router();
+  const controller: Controller = new controllerClass();
+
+  const routes = getMethodsWithMetadata(controllerClass);
+
+  routes.forEach(({ httpMethod, callbackPropertyName, path }) => {
+    const method = router[httpMethod].bind(router);
+    const callback = controller[callbackPropertyName];
+
+    method(path, callback.bind(controller));
+  });
+
+  app.use(controller.prefix, router);
 });
 
 app.use(errorHandler);
--- src/interfaces/controller.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-
-export interface Controller {
-  prefix: string;
-  router: Router;
-}
