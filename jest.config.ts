4491c32805753880da70fafbb9350d9d685f28e2
Jorge <jorgevitorp9@gmail.com>
2024-11-23 10:26:27 -0300

    🚧 chore: jest config file

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+};
+
+export default config;
