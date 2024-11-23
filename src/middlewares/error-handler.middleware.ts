69cd1da3e570299881fdbb0e60a402a8af566515
Jorge <jorgevitorp9@gmail.com>
2024-11-23 08:31:02 -0300

    📦 refactor(error-handler.middleware): next param to express understand that as a middleware

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,14 +1,19 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../classes/api-error";
 import { HttpStatusCode } from "axios";
 
-export function errorHandler(error: ApiError, req: Request, res: Response) {
+export function errorHandler(
+  error: ApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const { statusCode, message, type } = error;
   const responseObj: { [key: string]: string } = { message, type };
 
   console.log(error);
 
   res
-    .status(statusCode || HttpStatusCode.InternalServerError)
+    ?.status(statusCode || HttpStatusCode.InternalServerError)
     .json(responseObj);
 }
