315ef9495130c54eb0e2dd0c5308df4eb8c9184d
Jorge <jorgevitorp9@gmail.com>
2024-11-27 12:05:16 -0300

    🧪 test(downloadSong.controller.spec.ts): tests to all case of downloadSong.controller

---
+++ src/controllers/downloadSong/downloadSong.controller.spec.ts
@@ -0,0 +1,53 @@
+import { Request } from "express";
+import { DownloadSongController } from "./downloadSong.controller";
+
+import fs from "fs";
+import { ApiError } from "../../classes/api-error";
+
+const FILE_NAME = "/folder/output-file";
+
+jest.mock("fs");
+jest.mock("express");
+jest.mock("../../decorators/post.decorator", () => ({
+  Post: jest
+    .fn()
+    .mockImplementation(() => (target, key, descriptor) => descriptor),
+}));
+jest.mock("../../services/downloadSong/downloadSong.service", () => {
+  return {
+    DownloadSongService: jest.fn().mockImplementation(() => ({
+      downloadSong: jest.fn().mockResolvedValue(FILE_NAME),
+    })),
+  };
+});
+
+describe("DownloadSongController", () => {
+  let downloadSongController: DownloadSongController;
+
+  const mockRequestAndResponse = (videoId: string) => ({
+    req: { body: { videoId } } as Request,
+    res: {
+      sendFile: jest.fn((filePath: string, callback: () => void) => callback()),
+    } as any,
+  });
+
+  beforeEach(() => {
+    downloadSongController = new DownloadSongController();
+  });
+
+  it("should call res.sendFile and fs.rmSync", async () => {
+    const { req, res } = mockRequestAndResponse("XqZsoesa55w");
+    await downloadSongController.downloadVideo(req, res);
+
+    expect(res.sendFile).toHaveBeenCalledWith(FILE_NAME, expect.any(Function));
+    expect(fs.rmSync).toHaveBeenCalledWith(FILE_NAME);
+  });
+
+  it("should throw api error when pass invalid id", async () => {
+    const { req, res } = mockRequestAndResponse("invalidId");
+
+    await expect(
+      downloadSongController.downloadVideo(req, res)
+    ).rejects.toThrow(ApiError);
+  });
+});
