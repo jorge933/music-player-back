6c005c4ed6bbf27d906a6e45f718fac474af6e4e
Jorge <jorgevitorp9@gmail.com>
2024-12-25 16:04:59 -0300

    🚧 chore(jest.config.ts): removed unnecessary files to test on collectCoverage

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,13 @@ import type { Config } from "jest";
 const config: Config = {
   collectCoverage: true,
   coverageDirectory: "coverage",
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/index.ts",
+    "!src/decorators/get.decorator.ts",
+    "!src/decorators/post.decorator.ts",
+  ],
 };
 
 export default config;
